d4fa9520723c97b22a243b0b56fcff44c272d5f6
Diastray <evanandmir170@gmail.com>
2025-08-21 17:31:12 +0900

    DNA Problem solved

---
+++ bioinfo_stronghold/DNA.py
@@ -0,0 +1,18 @@
+f = open("C:/projects/code/Rosalind/bioinfo_stronghold/rosalind_dna.txt", "r")
+seq = f.readline().strip()
+
+base = [0, 0, 0, 0]
+for i in range(0, len(seq)):
+    if seq[i] == 'A':
+        base[0] = base[0] + 1
+    elif seq[i] == 'C':
+        base[1] = base[1] + 1
+    elif seq[i] == 'G':
+        base[2] = base[2] + 1
+    elif seq[i] == 'T':
+        base[3] = base[3] + 1
+
+print(*base)
+#print(sum(base))
+#print(len(seq))
+f.close()
